28a766594c19c762c5f3b2a3dc8fb1333082cdee
ravi <ravi@vytec.com>
2023-10-10 09:30:17 +0530

    updated

--- example/backend.tf
+++ example/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstateRG"
-    storage_account_name = "tfstatestorageacc091123"
+    storage_account_name = "tftstatestgacc10102023"
     container_name       = "tfstate"
     key                  = "dev/terraform.tfstate"
   }
--- tfstatestorageaccount/resources.tf
+++ tfstatestorageaccount/resources.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "tf" {
 }
 
 resource "azurerm_storage_account" "tf" {
-  name                     = "tftstatestorageaccount10102023"
+  name                     = "tftstatestgacc10102023"
   resource_group_name      = azurerm_resource_group.tf.name
   location                 = azurerm_resource_group.tf.location
   account_tier             = "Standard"
