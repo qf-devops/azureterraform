732701f97bd34d8e3cb1ba51b54a609efaf1d294
ravi <ravi@vytec.com>
2023-10-11 13:00:47 +0530

    upodated

--- example/main.tf
+++ example/main.tf
@@ -1,29 +1,33 @@
 
-resource "azurerm_resource_group" "tf" {
+resource "azurerm_resource_group" "rg" {
   name     = var.rgname
   location = var.location
 }
-module "abc"{
-  source = "./modules/storageaccount"
-  rgname = var.rgname
+resource "azurerm_resource_group" "rg1" {
+  name     = "stgerg"
   location = var.location
-  storageaccountname = var.storageaccountname
-  queuename = var.queuename
-  tablename = var.tablename
-  container_list = var.container_list
-  fileshare_list = var.fileshare_list
 }
-module "computing1"{
-  source = "./modules/computing"
-}
+# module "abc"{
+#   source = "./modules/storageaccount"
+#   rgname = var.rgname
+#   location = var.location
+#   storageaccountname = var.storageaccountname
+#   queuename = var.queuename
+#   tablename = var.tablename
+#   container_list = var.container_list
+#   fileshare_list = var.fileshare_list
+# }
+# module "computing1"{
+#   source = "./modules/computing"
+# }
 
-module "dev2"{
-  source = "./modules/storageaccount"
-  rgname = var.rgname
-  location = var.location
-  storageaccountname = var.storageaccountnamev1
-  queuename = var.queuename
-  tablename = var.tablename
-  container_list = var.container_list
-  fileshare_list = var.fileshare_list
-}
+# module "dev2"{
+#   source = "./modules/storageaccount"
+#   rgname = var.rgname
+#   location = var.location
+#   storageaccountname = var.storageaccountnamev1
+#   queuename = var.queuename
+#   tablename = var.tablename
+#   container_list = var.container_list
+#   fileshare_list = var.fileshare_list
+# }
