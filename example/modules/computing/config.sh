f63d1bb9251d88cf04d545c390b833b553e8b920
ravi <ravi@vytec.com>
2023-10-12 09:19:31 +0530

    updated

---
+++ example/modules/computing/config.sh
@@ -0,0 +1,40 @@
+
+
+
+sudo apt update
+sudo apt install nginx
+cd /var/www
+sudo mkdir tutorial
+cd tutorial
+sudo "${EDITOR:-vi}" index.html
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Hello, Nginx!</title>
+</head>
+<body>
+    <h1>Hello, Nginx!</h1>
+    <p>We have just configured our Nginx web server on Ubuntu Server!</p>
+</body>
+</html>
+
+cd /etc/nginx/sites-enabled
+sudo "${EDITOR:-vi}" tutorial
+
+server {
+       listen 81;
+       listen [::]:81;
+
+       server_name example.ubuntu.com;
+
+       root /var/www/tutorial;
+       index index.html;
+
+       location / {
+               try_files $uri $uri/ =404;
+       }
+}
+
+sudo service nginx restart
