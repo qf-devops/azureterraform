55d9bae2f830d960fa4b3a1c115d4136285aa992
ravi <ravi@vytec.com>
2023-10-14 10:37:14 +0530

    updated

---
+++ helm/main.tf
@@ -0,0 +1,27 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+
+resource "kubernetes_namespace" "example_namespace" {
+  metadata {
+    name = "example-namespace"
+  }
+}
+
+resource "helm_release" "example_helm_release" {
+  name       = "nginx-ingress-controller"
+  namespace  = kubernetes_namespace.example_namespace.metadata[0].name
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
